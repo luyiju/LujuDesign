3053d2154cca3fb063c065dc711e93c1843b7fc6
Luju <crystallu00421@gmail.com>
2018-02-25 22:25:56 +0800

    Luju
    2/25
    - 持續微調

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+
 // Loading
 document.onreadystatechange = subSomething;
 
@@ -27,7 +28,7 @@ function myFunction(e) {
 }
 
 //解除触摸锁定  
-document.removeEventListener("touchmove", myFunction); 
+document.removeEventListener("touchmove", myFunction);
 
 // Menu Bar
 function openNav() {
@@ -41,7 +42,6 @@ $('#toggle').click(function() {
     $(this).toggleClass('active');
     $('#overlay').toggleClass('open');
 });
-
 
 
 // Projects-filter
@@ -73,82 +73,8 @@ $(window).load(function() {
     });
 });
 
+
+
 // ====================================Projects Modal=========================================================================
-// ======Website===============/////////
-// Lime-Get the modal
-var modal = document.getElementById('LimeModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("Lime");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-// Starts-Get the modal
-// var modal = document.getElementById('StarsModal');
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("Starts");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks the button, open the modal
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-// ======Art Work===============/////////
-// Night-Get the modal
-// var modal = document.getElementById('NightModal');
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("Night");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks the button, open the modal
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
